aef4861771e1dabb5fdff88a7731191aa68d69e6
Jake Weinstein <jake@aospa.co>
2020-01-23 05:04:35 +0100

    power: Correct Legacy PowerHAL ifdef
    Change-Id: Ie9a8da381afdce071f3a27f7fdbecac2bf8cb722

--- power-vendor-board.mk
+++ power-vendor-board.mk
@@ -1,6 +1,7 @@
 # Disable Power HAL by default for legacy targets.
 # Devices can still opt in by setting TARGET_USES_NON_LEGACY_POWERHAL in BoardConfig.mk.
 # Conversely, recent chips, such as sm8150 with an old vendor can opt out.
-ifneq ($(call is-board-platform-in-list, msm8937 msm8952 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845 sdm710 trinket), true)
+ACCEPTED_PLATFORMS := $(filter-out $(TRINKET), $(UM_4_14_FAMILY))
+ifeq ($(call is-board-platform-in-list, $(ACCEPTED_PLATFORMS)), true)
 TARGET_USES_NON_LEGACY_POWERHAL ?= true
 endif
