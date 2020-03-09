bd683f89b01d36f965f1e8624618e4ed109d5378
Arian <arian.kulmer@web.de>
2020-03-09 22:19:56 +0100

    power: Include the vintf manifest on vendor builds
    Change-Id: Ic10b23bd3e30a63cbc67ad65bee312da7722650a

--- Android.mk
+++ Android.mk
@@ -161,6 +161,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable -DLEGACY_QCOM_POWERHAL
 ifneq ($(TARGET_OVERLAYS_POWERHAL), true)
 LOCAL_VENDOR_MODULE := true
+LOCAL_VINTF_FRAGMENTS := android.hardware.power@1.0-service.xml
 else
 LOCAL_VENDOR_OVERLAY_MODULE := true
 endif
@@ -176,6 +177,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 ifneq ($(TARGET_OVERLAYS_POWERHAL), true)
 LOCAL_VENDOR_MODULE := true
+LOCAL_VINTF_FRAGMENTS := android.hardware.power@1.2-service.xml
 else
 LOCAL_VENDOR_OVERLAY_MODULE := true
 endif
