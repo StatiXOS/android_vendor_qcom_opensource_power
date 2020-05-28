09e9db0143d1a33d7f6614e919b658c710bfd439
Anay Wadhera <awadhera@berkeley.edu>
2020-05-27 19:48:45 -0700

    power: clang-format
    Change-Id: I39ade4af917f4e999add23c2290d5e8292ad12e3

--- power-8974.c
+++ power-8974.c
@@ -28,24 +28,24 @@
  */
 #define LOG_NIDEBUG 0
 
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <dlfcn.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define LOG_TAG "QTI PowerHAL"
-#include <log/log.h>
 #include <hardware/hardware.h>
 #include <hardware/power.h>
+#include <log/log.h>
 
-#include "utils.h"
-#include "metadata-defs.h"
 #include "hint-data.h"
+#include "metadata-defs.h"
 #include "performance.h"
 #include "power-common.h"
+#include "utils.h"
 
 static int display_hint_sent;
 static int display_hint2_sent;
@@ -57,85 +57,88 @@ static int first_display_off_hint;
  * else:
  *     return false
  */
-static bool is_target_8974pro(void)
-{
-    int fd;
-    bool is_target_8974pro = false;
-    char buf[10] = {0};
+static bool is_target_8974pro(void) {
+  int fd;
+  bool is_target_8974pro = false;
+  char buf[10] = {0};
 
-    fd = open("/sys/devices/soc0/soc_id", O_RDONLY);
-    if (fd >= 0) {
-        if (read(fd, buf, sizeof(buf) - 1) == -1) {
-            ALOGW("Unable to read soc_id");
-            is_target_8974pro = false;
-        } else {
-            int soc_id = atoi(buf);
-            if (soc_id == 194 || (soc_id >= 208 && soc_id <= 218)) {
-                is_target_8974pro = true;
-            }
-        }
+  fd = open("/sys/devices/soc0/soc_id", O_RDONLY);
+  if (fd >= 0) {
+    if (read(fd, buf, sizeof(buf) - 1) == -1) {
+      ALOGW("Unable to read soc_id");
+      is_target_8974pro = false;
+    } else {
+      int soc_id = atoi(buf);
+      if (soc_id == 194 || (soc_id >= 208 && soc_id <= 218)) {
+        is_target_8974pro = true;
+      }
     }
-    close(fd);
-    return is_target_8974pro;
+  }
+  close(fd);
+  return is_target_8974pro;
 }
 
-int set_interactive_override(struct power_module *module, int on)
-{
-    char governor[80];
+int set_interactive_override(struct power_module *module, int on) {
+  char governor[80];
 
-    if (get_scaling_governor(governor, sizeof(governor)) == -1) {
-        ALOGE("Can't obtain scaling governor.");
-
-        return HINT_NONE;
-    }
-
-    if (!on) {
-        /* Display off. */
-        /*
-         * We need to be able to identify the first display off hint
-         * and release the current lock holder
-         */
-        if (is_target_8974pro()) {
-            if (!first_display_off_hint) {
-                undo_initial_hint_action();
-                first_display_off_hint = 1;
-            }
-            /* used for all subsequent toggles to the display */
-            if (!display_hint2_sent) {
-                undo_hint_action(DISPLAY_STATE_HINT_ID_2);
-                display_hint2_sent = 1;
-            }
-        }
-
-        if ((strncmp(governor, ONDEMAND_GOVERNOR, strlen(ONDEMAND_GOVERNOR)) == 0) &&
-                (strlen(governor) == strlen(ONDEMAND_GOVERNOR))) {
-            int resource_values[] = {MS_500, SYNC_FREQ_600, OPTIMAL_FREQ_600, THREAD_MIGRATION_SYNC_OFF};
-
-            if (!display_hint_sent) {
-                perform_hint_action(DISPLAY_STATE_HINT_ID,
-                        resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
-                display_hint_sent = 1;
-            }
-
-            return HINT_HANDLED;
-        }
-    } else {
-        /* Display on */
-        if (is_target_8974pro() && display_hint2_sent) {
-            int resource_values2[] = {CPUS_ONLINE_MIN_2};
-            perform_hint_action(DISPLAY_STATE_HINT_ID_2,
-                    resource_values2, sizeof(resource_values2)/sizeof(resource_values2[0]));
-            display_hint2_sent = 0;
-        }
-
-        if ((strncmp(governor, ONDEMAND_GOVERNOR, strlen(ONDEMAND_GOVERNOR)) == 0) &&
-                (strlen(governor) == strlen(ONDEMAND_GOVERNOR))) {
-            undo_hint_action(DISPLAY_STATE_HINT_ID);
-            display_hint_sent = 0;
-
-            return HINT_HANDLED;
-        }
-    }
+  if (get_scaling_governor(governor, sizeof(governor)) == -1) {
+    ALOGE("Can't obtain scaling governor.");
 
     return HINT_NONE;
+  }
+
+  if (!on) {
+    /* Display off. */
+    /*
+     * We need to be able to identify the first display off hint
+     * and release the current lock holder
+     */
+    if (is_target_8974pro()) {
+      if (!first_display_off_hint) {
+        undo_initial_hint_action();
+        first_display_off_hint = 1;
+      }
+      /* used for all subsequent toggles to the display */
+      if (!display_hint2_sent) {
+        undo_hint_action(DISPLAY_STATE_HINT_ID_2);
+        display_hint2_sent = 1;
+      }
+    }
+
+    if ((strncmp(governor, ONDEMAND_GOVERNOR, strlen(ONDEMAND_GOVERNOR)) ==
+         0) &&
+        (strlen(governor) == strlen(ONDEMAND_GOVERNOR))) {
+      int resource_values[] = {MS_500, SYNC_FREQ_600, OPTIMAL_FREQ_600,
+                               THREAD_MIGRATION_SYNC_OFF};
+
+      if (!display_hint_sent) {
+        perform_hint_action(DISPLAY_STATE_HINT_ID, resource_values,
+                            sizeof(resource_values) /
+                                sizeof(resource_values[0]));
+        display_hint_sent = 1;
+      }
+
+      return HINT_HANDLED;
+    }
+  } else {
+    /* Display on */
+    if (is_target_8974pro() && display_hint2_sent) {
+      int resource_values2[] = {CPUS_ONLINE_MIN_2};
+      perform_hint_action(DISPLAY_STATE_HINT_ID_2, resource_values2,
+                          sizeof(resource_values2) /
+                              sizeof(resource_values2[0]));
+      display_hint2_sent = 0;
+    }
+
+    if ((strncmp(governor, ONDEMAND_GOVERNOR, strlen(ONDEMAND_GOVERNOR)) ==
+         0) &&
+        (strlen(governor) == strlen(ONDEMAND_GOVERNOR))) {
+      undo_hint_action(DISPLAY_STATE_HINT_ID);
+      display_hint_sent = 0;
+
+      return HINT_HANDLED;
+    }
+  }
+
+  return HINT_NONE;
 }
