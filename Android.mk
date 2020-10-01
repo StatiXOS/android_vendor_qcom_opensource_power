aa7e248f1061731f04a9e90c63ca9c42ec7ce435
LuK1337 <priv.luk@gmail.com>
2020-10-01 14:13:46 +0200

    power: Allow device specific hooks for setMode, isModeSupported
    * Some devices may want to implement custom hooks
    
    Change-Id: Icb2d66471ec649a69b1e69849fd86282775052cb

--- Android.mk
+++ Android.mk
@@ -76,6 +76,11 @@ ifeq ($(call is-board-platform-in-list,msmnile), true)
 LOCAL_SRC_FILES += power-msmnile.c
 endif
 
+ifneq ($(TARGET_POWERHAL_MODE_EXT),)
+    LOCAL_CFLAGS += -DMODE_EXT
+    LOCAL_SRC_FILES += ../../../../$(TARGET_POWERHAL_MODE_EXT)
+endif
+
 ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
     LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
 endif
